a928522ae8d35cb9163f1206d51c77f69a4b34dd
fieldOfView <aldo@fieldofview.com>
2012-01-01 22:46:27 +0100

    Don't "black out" uv (0,0), but instead copy alpha from uv map

--- uvPlayer/resources/effect_frag.glsl
+++ uvPlayer/resources/effect_frag.glsl
@@ -10,12 +10,10 @@ uniform bool flipv;
 
 void main()
 {
-	vec2 uv = texture2D( map, gl_TexCoord[0].xy ).rg;
-	if (uv.x > 0.0 && uv.y > 0.0) {
-		if ( flipv ) 
-			uv.y = 1.0-uv.y;
-		
-		gl_FragColor.rgb = texture2DRect( frame, uv.xy * frameSize ).rgb;	
-	}
-	gl_FragColor.a = 1.0;	
+	vec4 uv = texture2D( map, gl_TexCoord[0].xy );
+	if ( flipv ) 
+		uv.y = 1.0-uv.y;
+
+    gl_FragColor.rgb = texture2DRect( frame, uv.xy * frameSize ).rgb;	
+    gl_FragColor.a = uv.a;	
 }
