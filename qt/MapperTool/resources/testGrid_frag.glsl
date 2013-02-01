204e33a4c3a4d337d5683d03772bd0c14948740a
fieldOfView <aldo@fieldofview.com>
2013-02-01 13:37:54 +0100

    Draw a circle in the displaytexture grid

--- qt/MapperTool/resources/testGrid_frag.glsl
+++ qt/MapperTool/resources/testGrid_frag.glsl
@@ -6,7 +6,8 @@ uniform float width;
 void main(void)
 {
     vec3 colorX = mix(colorFG, colorBG, step(fract(gl_TexCoord[0].x*cells - width/2.), 1.-width));
-    gl_FragColor.rgb = mix(colorFG, colorX, step(fract(gl_TexCoord[0].y*cells - width/2.), 1.-width));
+    vec3 colorY = mix(colorFG, colorX, step(fract(gl_TexCoord[0].y*cells - width/2.), 1.-width));
+    gl_FragColor.rgb = mix(colorFG, colorY, step(fract(2.*sqrt(pow(gl_TexCoord[0].x-.5,2.)+pow(gl_TexCoord[0].y-.5,2.))), 1.-(2.*width/cells)));
 
     gl_FragColor.a = 1.0;
 }
